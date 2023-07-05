dd9887fd5af13db8c0ff8c2f60d3eeae10635fd1
Guilherme-07062002 <guilherme762002@gmail.com>
2023-07-04 22:29:38 -0300

    fake repository

--- src/domain/repositories/task-repository.ts
+++ src/domain/repositories/task-repository.ts
@@ -2,5 +2,5 @@ import { Task } from './../entities/task';
 import { createTaskDTO } from './../dtos/task-dtos';
 
 export interface TaskRepository {
-    create(data: createTaskDTO): Promise<Task | undefined>
+    create(data: createTaskDTO): Promise<Task>
 }
---
+++ src/infra/repositories/fake/fake-task-repository.ts
@@ -0,0 +1,9 @@
+import { createTaskDTO } from '../../../domain/dtos';
+import { Task } from '../../../domain/entities';
+import { TaskRepository } from './../../../domain/repositories/task-repository';
+
+export class FakeUserRepository implements TaskRepository {
+    async create(data: createTaskDTO): Promise<Task> {
+        return new Task(data.id, data.description);
+    }
+}
