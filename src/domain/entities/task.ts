9f5111fd9b309e6f3bb009900511347b24af25b2
Guilherme-07062002 <guilherme762002@gmail.com>
2023-07-04 22:00:19 -0300

    entitie task

---
+++ src/domain/entities/task.ts
@@ -0,0 +1,9 @@
+export class Task {
+    public id: number;
+    public description: string;
+
+    constructor(id: number, description: string) {
+        this.id = id
+        this.description = description
+    }
+}
