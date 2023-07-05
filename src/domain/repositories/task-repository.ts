85a0bba8787628bbe16bffa8178dbfe01ac32891
Guilherme-07062002 <guilherme762002@gmail.com>
2023-07-04 22:07:52 -0300

    task create repository

---
+++ src/domain/repositories/task-repository.ts
@@ -0,0 +1,6 @@
+import { Task } from './../entities/task';
+import { createTaskDTO } from './../dtos/task-dtos';
+
+export interface TaskRepository {
+    create(data: createTaskDTO): Promise<Task | undefined>
+}
