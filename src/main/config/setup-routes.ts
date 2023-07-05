2b6936d3cae3dfdeae1f908bd1501583ea808d2a
Guilherme-07062002 <guilherme762002@gmail.com>
2023-07-04 22:38:48 -0300

    setup routes

---
+++ src/main/config/setup-routes.ts
@@ -0,0 +1,6 @@
+import { Router } from "express"
+import taskRoutes from "../routes/task-routes"
+
+export default (router: Router): void => {
+    taskRoutes('/task', router)
+}   
---
+++ src/main/routes/task-routes.ts
@@ -0,0 +1,5 @@
+import { Router } from 'express';
+
+export default (prefix: string, router: Router): void => {
+    router.post(`${prefix}/`,)
+}
