df763ce2f44cef9a4fcd68c008550675f61f6f64
Guilherme-07062002 <guilherme762002@gmail.com>
2023-07-04 22:22:17 -0300

    create task controller

---
+++ src/interface/controllers/create-task-controller.ts
@@ -0,0 +1,24 @@
+import { createTaskDTO } from './../../domain/dtos/task-dtos';
+import { UseCase } from './../../domain/ports/usecase';
+import { Request, Response } from './../../domain/ports/http';
+import { Controller } from './../../domain/ports/controller';
+import { created } from '../adapters';
+
+namespace Request {
+    export type Body = {
+        id: number,
+        description: string
+    }
+}
+
+export class CreateUserController implements Controller {
+    constructor(
+        private readonly useCase: UseCase
+    ) { }
+    async handle(request: Request<Request.Body>): Promise<Response> {
+        const body = request.body
+
+        const res = await this.useCase.execute(body as createTaskDTO)
+        return created(res)
+    }
+}
