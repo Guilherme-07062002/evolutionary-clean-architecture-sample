4a42e5d7c586062c1b4b4d22d5839e56eea8eaa9
Guilherme-07062002 <guilherme762002@gmail.com>
2023-12-01 15:28:23 -0300

    test: task entity

---
+++ tests/entities/task-entity.test.ts
@@ -0,0 +1,15 @@
+import { describe, expect, test } from "vitest";
+import { Task } from "../../src/domain/entities/task";
+
+describe("test task entity", () => {
+  test("should create a task", () => {
+    const task = new Task(1, "any_description");
+    expect(task).toBeDefined();
+  });
+
+  test("should return a json", () => {
+    const task = new Task(1, "any_description");
+    const json = task.toJSON();
+    expect(json).toEqual({ id: 1, description: "any_description" });
+  });
+});
