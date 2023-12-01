ce85c65e35b03c9292e53438d5a475908c53c33d
Guilherme-07062002 <guilherme762002@gmail.com>
2023-12-01 15:25:54 -0300

    test: update task usecase

---
+++ tests/usecases/update-task-usecase.test.ts
@@ -0,0 +1,33 @@
+import { mock } from "vitest-mock-extended";
+import { TaskRepository } from "../../src/domain/repositories";
+import { UpdateTaskUsecase } from "../../src/application";
+import { describe, expect, test } from "vitest";
+import { EntityNotFoundError } from "../../src/domain/errors";
+
+const makeSut = () => {
+  const repo = mock<TaskRepository>();
+  const usecase = new UpdateTaskUsecase(repo);
+  return { repo, usecase };
+};
+
+describe("test update task usecase", () => {
+  test("should update a task", async () => {
+    const { repo, usecase } = makeSut();
+    repo.update.mockResolvedValueOnce(true);
+    const response = await usecase.execute({
+      id: 1,
+      new_description: "any_description",
+    });
+    expect(response).toBeTruthy();
+  });
+
+  test("should throw error if not found", async () => {
+    const { repo, usecase } = makeSut();
+    repo.update.mockResolvedValueOnce(new EntityNotFoundError("Task"));
+    const response = await usecase.execute({
+      id: 1,
+      description: "any_description",
+    });
+    expect(response).toBeInstanceOf(EntityNotFoundError);
+  });
+});
