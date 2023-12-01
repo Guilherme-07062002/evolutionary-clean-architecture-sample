50ee8bf4771e26670718113273f3dd55fa5d9e54
Guilherme-07062002 <guilherme762002@gmail.com>
2023-12-01 15:21:47 -0300

    test: list tasks usecase

---
+++ tests/usecases/list-tasks-usecase.test.ts
@@ -0,0 +1,19 @@
+import { mock } from "vitest-mock-extended";
+import { TaskRepository } from "../../src/domain/repositories";
+import { ListTasksUseCase } from "../../src/application";
+import { describe, expect, test } from "vitest";
+
+const makeSut = () => {
+  const repo = mock<TaskRepository>();
+  const usecase = new ListTasksUseCase(repo);
+  return { repo, usecase };
+};
+
+describe("test list tasks usecase", () => {
+  test("should list tasks", async () => {
+    const { repo, usecase } = makeSut();
+    repo.list.mockResolvedValueOnce([]);
+    const response = await usecase.execute();
+    expect(response).toEqual([]);
+  });
+});
